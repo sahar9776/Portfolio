3bef657951886181eeecb5c1679367b55b00c312
sahar9776 <sahar.samadi1376@gmail.com>
2025-07-15 12:35:43 +0430

    new

--- src/Components/Introduction.tsx
+++ src/Components/Introduction.tsx
@@ -26,7 +26,7 @@ function Introduction() {
           }}
           src="/images/miss.jpg"
           alt=""
-          className="max-w-full lg:w-96 h-auto absolute bottom-[170px] left-[50%] -translate-x-[50%]
+          className="max-w-full lg:w-96 h-auto absolute bottom-20 left-[50%] -translate-x-[50%]
            lg:bottom-10 lg:left-40 lg:translate-x-0 lg:rounded-sm"
         />
 
@@ -42,14 +42,14 @@ function Introduction() {
               key={index}
               variants={childVariants}
               className="font-extrabold uppercase text-8xl lg:text-9xl
-             text-white lg:text-black/80 ms-12 lg:ms-[220px]"
+             text-white lg:text-black/80 ms-14 lg:ms-[220px]"
             >
               {item}
             </motion.h2>
           ))}
         </motion.div>
 
-        <div className="w-full lg:w-auto absolute lg:right-24 top-[394px] lg:top-96 px-5 py-48 lg:p-0 text-start">
+        <div className="w-full lg:w-auto absolute lg:right-24 top-[440px] lg:top-96 px-5 py-48 lg:p-0 text-start">
           <Title
             title="Front-end Web Developer"
             className="text-xl lg:text-4xl font-bold text-white mt-2 lg:mt-0"
